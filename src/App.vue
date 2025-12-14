55ac5225bafba6a8f170ef3ae695d2b6ee55bb85
luizgavan-boop <luizgavan@gmail.com>
2025-12-14 17:29:42 +0800

    feat:landing page

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Hero from './components/Hero.vue'
+import About from './components/About.vue'
+import Skills from './components/Skills.vue'
+import FooterSection from './components/FooterSection.vue'
+</script>
+
+<template>
+<Hero />
+<About />
+<Skills />
+<FooterSection />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FooterSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="min-h-screen flex flex-col justify-center items-center bg-yellow-500">
+
+
+
+
+       <h1 class="text-3xl text-black italic mb-10">WELCOME TO THE LAND OF CAS</h1>
+
+       <div class="border-2  border-yellow-500 p-4 
+       rounded-md text-black">
+        This is a Sunsetz bordered box
+       </div>
+
+     </div>
+</template>
